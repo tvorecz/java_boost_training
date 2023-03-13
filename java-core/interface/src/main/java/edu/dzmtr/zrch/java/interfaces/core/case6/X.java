730a7480bad9a1feb511dadee90ac3d7d969e8b5
Dzmitry Zorych <d.zorych@eisgroup.com>
2023-03-13 11:43:08 +0200

    java core interfaces exercises

---
+++ java-core/interface/src/main/java/edu/dzmtr/zrch/java/interfaces/core/case6/X.java
@@ -0,0 +1,10 @@
+package edu.dzmtr.zrch.java.interfaces.core.case6;
+
+/**
+ * TODO description
+ *
+ * @author Dzmitry_Zorych
+ */
+public interface X<T> {
+    void m(T arg);
+}
---
+++ java-core/interface/src/main/java/edu/dzmtr/zrch/java/interfaces/core/case6/Y.java
@@ -0,0 +1,10 @@
+package edu.dzmtr.zrch.java.interfaces.core.case6;
+
+/**
+ * TODO description
+ *
+ * @author Dzmitry_Zorych
+ */
+public interface Y<T> {
+    void m(T arg);
+}
